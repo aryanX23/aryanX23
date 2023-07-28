3c6c41ca30ab15333201320f488e1a8c89afdb8e
Aryan Rai <aryanx23@pop-os.localdomain>
2023-07-29 01:34:46 +0530

    making changes in routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -9,25 +9,25 @@ import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const AnimatedRoutes = withRouter(({ location }) => (
-  <TransitionGroup>
-    <CSSTransition
-      key={location.key}
-      timeout={{
-        enter: 400,
-        exit: 400,
-      }}
-      classNames="page"
-      unmountOnExit
-    >
-      <Routes location={location}>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </CSSTransition>
-  </TransitionGroup>
+    <TransitionGroup>
+        <CSSTransition
+            key={location.key}
+            timeout={{
+                enter: 400,
+                exit: 400,
+            }}
+            classNames="page"
+            unmountOnExit
+        >
+            <Routes location={location}>
+                <Route exact path="/aryanX23/" element={<Home />} />
+                <Route path="/aryanX23/about" element={<About />} />
+                <Route path="/aryanX23/portfolio" element={<Portfolio />} />
+                <Route path="/aryanX23/contact" element={<ContactUs />} />
+                <Route path="*" element={<Home />} />
+            </Routes>
+        </CSSTransition>
+    </TransitionGroup>
 ));
 
 function AppRoutes() {
